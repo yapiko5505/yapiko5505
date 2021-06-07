e9a344d218e11fd3a5258c7809ae41accbd88301
yapiko5505 <84828867+yapiko5505@users.noreply.github.com>
2021-06-07 09:48:45 +0900

    Add files via upload

---
+++ twiiterclone/views/sign-in.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="discription" content="ホーム画面です">
+    <link rel="icon" href="\TwittewrClone\views\img\logo-twitterblue.svg">
+    <!--Bootstrap CSS  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link href="\TwitterClone\views\css\style.css" rel="stylesheet">
+    
+    <title>ログイン画面/twitterクローン</title>
+    <meta name="description" content="ログイン画面です">
+</head>
+
+<body class="signup text-center">
+    <main class="form-signup">
+        <form action="sign-in.php" metsod="post">
+            <img src="/twitterClone/views/img/logo-white.svg" alt="" class="logo-white">
+            <h1>Twitterクローンにログイン</h1>
+            <input type="email" class="form-control" name="email" placeholder="メールアドレス" require autofocus>
+            <input type="password" class="form-control" name="password" placeholder="パスワード" require>
+            <button class="w-100 btn btn-lg" type="submit">ログインする</button>
+            <p class="mt-3 mb-2"><a href="sign-up.php">会員登録する</a></p>
+            <p class="mt-2 mb-3 text-muted">&copy; 2021</p>
+
+
+        </form>
+    </main>
+
+</body>
+
+</html>
