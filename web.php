fece857a9ae1f928576afb42a065fa01ad6429b5
yapiko5505 <84828867+yapiko5505@users.noreply.github.com>
2021-06-08 18:03:29 +0900

    Add files via upload

---
+++ web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+Route::get('/tasks', [App\Http\Controllers\TaskController::class,'index'])->name('tasks');
+Route::post('/task', [App\Http\Controllers\TaskController::class,'store'])->name('task');
+Route::delete('/task/{task}', [App\Http\Controllers\TaskController::class,'destory'])->name('/task/{task}');
+
